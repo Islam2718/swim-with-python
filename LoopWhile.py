56573049e4c40bb1847ac490c13ab75d88eb9666
Islam2718 <mislamhn@gmail.com>
2023-02-10 16:15:51 +0600

    find even and odd between two number

--- LoopWhile.py
+++ LoopWhile.py
@@ -22,10 +22,20 @@
 #     i += 1
 
 # print even & Odd number between 1 to 100
-i = 1
-while i <= 100:
-    if i % 2 == 0:
-        print(str(i)+" Is Event")
+# i = 1
+# while i <= 100:
+#     if i % 2 == 0:
+#         print(str(i)+" Is Event")
+#     else:
+#         print(str(i) + " Is Odd")
+#     i += 1
+
+# print even & Odd between two number
+fromStart = int(input("Enter Start Value:"))
+toEnd = int(input("Enter End Value: "))
+while fromStart <= toEnd:
+    if int(fromStart) % 2 == 0:
+        print(str(fromStart)+" Is Event")
     else:
-        print(str(i) + " Is Odd")
-    i += 1
+        print(str(fromStart) + " Is Odd")
+    fromStart += 1
